5647322d7f798d0d5b75267b2b63cc59df17bf99
anthonymolinari <anthonymolinari106@gmail.com>
2024-12-05 12:24:57 -0800

    updates

--- deployments/test_deployment.py
+++ deployments/test_deployment.py
@@ -7,7 +7,7 @@ SOURCE_REPO = "https://github.com/anthonymolinari/prefect-flows.git"
 if __name__ == "__main__":
     flow.from_source(
         source=SOURCE_REPO,
-        entrypoint="test_flow.py:hello_universe",
+        entrypoint="flows/test_flow.py:hello_universe",
     ).deploy(
         name="my-first-deployment",
         work_pool_name="my-work-pool",

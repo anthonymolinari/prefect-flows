305849794fe79b95ffed3893bec0af545c62052f
anthonymolinari <anthonymolinari106@gmail.com>
2024-12-05 09:24:10 -0800

    add additional task

--- test_flow.py
+++ test_flow.py
@@ -6,8 +6,14 @@ def say_hello(name: str):
     print(f"Hello {name}!")
 
 
+@task(log_prints=True)
+def say_goodbye(name: str):
+    print(f"Goodbye {name}!")
+
+
 @flow
 def hello_universe():
     names = ["Jim", "John", "Joe"]
     for name in names:
         say_hello(name)
+        say_goodbye(name)
